ab406aac3d664d69d66ea35cd6d55dca7c9f8830
JOONHONG KIM <zmellonz@naver.com>
2019-11-29 09:31:28 +0900

    이것저것 끌어다가 최대한 해봄

--- hw9/subway.h
+++ hw9/subway.h
@@ -1,14 +1,46 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include<vector>
+#include<cstdio>
+#include<math.h>
+
+#define MAX_S 900
+#define INF 9999
 //필요한것 탐색,최단길이 알고리즘
 using namespace std;
-
-class graph {
-private:
-	int n;//노드 개수
-	int	weight[][];
-	string name;
-public:
-
+struct Key {
+	Key(int l,string n):line(l),name(n){}
+	int line;	//몇호선
+	string name;	//역이름
 };
+struct Vertex {
+	Key key;
+	Vertex* next;
+};
+
+struct Edge{
+	int weight;
+	Vertex* from;
+	Vertex* target;
+};
+
+class Graph {
+private:
+	int v;	//vertex 개수
+	int e;	//edge 개수
+	int size;
+	Vertex* vertices; //정점 목록
+public:
+	Graph(int v) {
+		this->v = v;
+		int size = 0;
+	}
+	void insertVertex(int, string);
+	void insertEdge()
+};
+
+
+void Graph::insertVertex(int l, string n) {
+
+}
