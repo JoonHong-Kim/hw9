e34778948b229df341fbfe301227eb641b2e214e
JOONHONG KIM <zmellonz@naver.com>
2019-11-29 11:24:05 +0900

    인접리스트를 짜고 우선 역들은 key값으로 다 받아놨다

--- hw9/hw9.cpp
+++ hw9/hw9.cpp
@@ -1,13 +1,18 @@
 #include"subway.h"
 #include<fstream>
 
+#define MAX_S 1000
+
 
 using namespace std;
 int main(int argc, char* argv[])
 {
 	int numLine;
+	int numStation;
 	int line1, line2;
+	int index = 0;
 	string src, dst;
+	Key station[MAX_S];
 	if (argc != 3)
 	{
 		cerr << "Argument Count is " << argc << endl << "Argument must be " << argc << endl;
@@ -19,10 +24,16 @@ int main(int argc, char* argv[])
 		cerr << argv[1] << " open failed" << endl;
 		return 1;
 	}
-	fin >> numLine;
-	//데이터 받는 과정
-	//…
+	fin >> numStation;
+	for (int i = 1; i <= numStation; i++) {// 호선수만큼 반복 i 는 호선수
+		fin >> numStation;//호선에 몇개역 있는지 받기
+		for (int j = 0; i < numStation; j++,index++) { //한 호선의 개수 만큼 반복
+			station[index].line = i;
+			fin >> station[index].name;
+		}
+	}
 	fin.close();
+
 	fstream fin2(argv[2]);
 	if (!fin2)
 	{
@@ -35,7 +46,7 @@ int main(int argc, char* argv[])
 	cout << "출발역 : " << src << "(line" << line1 << ")" << endl;
 	cout << "도착역 : " << dst << "(line" << line2 << ")" << endl;
 	cout << "============================================" << endl;
-	//결과 출력
+	
 	return 0;
 
 }
--- hw9/subway.h
+++ hw9/subway.h
@@ -9,11 +9,8 @@
 #define INF 9999
 //필요한것 탐색,최단길이 알고리즘
 using namespace std;
-struct Key {
-	Key(int l,string n):line(l),name(n){}
-	int line;	//몇호선
-	string name;	//역이름
-};
+
+/*
 struct Vertex {
 	Key key;
 	Vertex* next;
@@ -25,17 +22,20 @@ struct Edge{
 	Vertex* target;
 };
 
+struct Key {
+	Key(int l, string n) :line(l), name(n) {}
+	int line;	//몇호선
+	string name;	//역이름
+};
+
 class Graph {
 private:
 	int v;	//vertex 개수
 	int e;	//edge 개수
-	int size;
-	Vertex* vertices; //정점 목록
+	Key verticies[MAX_S]; //호선+이름
+	Vertex adjlist[MAX_S]; //인접리스트
 public:
-	Graph(int v) {
-		this->v = v;
-		int size = 0;
-	}
+	
 	void insertVertex(int, string);
 	void insertEdge()
 };
@@ -44,3 +44,60 @@ public:
 void Graph::insertVertex(int l, string n) {
 
 }
+*/
+struct Key {
+	Key(int l=0, string n=0,int in=0) :line(l), name(n),index(in) {}
+	int line;	//몇호선
+	string name;	//역이름
+	int index;		//넣을때 index값도 넣어줘야함
+};
+class ChainNode
+{
+private:
+	Key data;
+	ChainNode* link;
+	int weight;
+public:
+	ChainNode(Key element, int w = 0,ChainNode* next = NULL) :data(element), link(next), weight(w) {  }
+	Key getData(){
+		return data;
+	}
+
+	ChainNode* Link(){
+		return link;
+	}
+	friend class AdjList;
+	friend class LinkedGraph;
+	friend ostream& operator<<(ostream& os, LinkedGraph& lg);
+};
+
+
+
+class AdjList{
+	ChainNode* first;
+	int weight;
+	friend class LinkedGraph;
+	friend ostream& operator<<(ostream& os, LinkedGraph& lg);
+};
+
+class LinkedGraph{
+private:
+	AdjList* list;
+	int n;
+public:
+	LinkedGraph(int number) :n(number){
+		list = new AdjList[n];
+		for (int i = 0; i < n; i++)
+			list[i].first = NULL; //초기화
+	}
+	void linkEdge(Key k1, Key k2,int weight){ //꼭지점과 꼭지점을 이어준다
+		ChainNode* newNode = new ChainNode(k2,weight);
+		newNode->link = list[k1.index].first; //k2의 다음을 k1으로
+		list[k1.index].first = newNode;
+
+		newNode = new ChainNode(k1,weight);
+		newNode->link = list[k2.index].first; //k1다음을 k2로
+		
+	}
+};
+
